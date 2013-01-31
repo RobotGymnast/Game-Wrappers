8b41a2bc3615be47cff9cc7314f2bdbab34be907
Ben Foppa <benjamin.foppa@gmail.com>
2013-01-31 10:58:29 -0500

    Stripped down event interface

--- src/Wrappers/Events.hs
+++ src/Wrappers/Events.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS -fno-cse #-}
 {-# LANGUAGE NoImplicitPrelude
            #-}
 -- | Abstract event wrapper
@@ -5,11 +6,10 @@ module Wrappers.Events ( Event (..)
                        , Button (..)
                        , GLFW.Key (..)
                        , GLFW.MouseButton (..)
-                       , EventConstraint (..)
                        , Size (..)
                        , ButtonState (..)
-                       , EventPoller
-                       , createEventPoller
+                       , setUpEvents
+                       , popEvent
                        ) where
 
 import Prelewd
@@ -17,9 +17,7 @@ import Prelewd
 import IO
 
 import Text.Show
-
-import Storage.List
-import Storage.Queue
+import System.IO.Unsafe
 
 import Wrappers.OpenGL
 import Wrappers.STM
@@ -47,58 +45,31 @@ data Button = KeyButton GLFW.Key
             | MouseButton GLFW.MouseButton
     deriving (Eq, Show)
 
--- | Filters to use when getting events with `poll`
-data EventConstraint = ButtonEvents (Maybe Button) (Maybe ButtonState)
-                     | MouseMoveEvents
-                     | ResizeEvents
-                     | RefreshEvents
-                     | CloseEvents
-    deriving (Eq, Show)
+events :: TQueue Event
+events = unsafePerformIO newTQueueIO
 
--- | Poll for specific types of events
-type EventPoller = [EventConstraint] -- ^ Event types to filter for
-                 -> IO [Event]
+{-# NOINLINE events #-}
 
 -- | Push an event into the shared variable.
-addEvent :: TVar (Queue Event) -> Event -> IO ()
-addEvent = void . atomically <$$> modifyTVar <%> enq
+addEvent :: Event -> IO ()
+addEvent = atomically . writeTQueue events
 
 -- | Set up a queued event system
 -- `GLFW.initialize` must have been called
-createEventPoller :: IO EventPoller
-createEventPoller = atomically (newTVar mempty) >>= go
+setUpEvents :: IO ()
+setUpEvents = filter id (io GLFW.initialize) >> io setCallbacks
     where
-        go events = mfilter (\x->x) (io GLFW.initialize) >> io (setCallbacks events) $> poll events
-
         toSize = on Size fromIntegral
         toPos = on Position fromIntegral
 
-        setCallbacks events = sequence_
-            [ GLFW.setWindowCloseCallback $ True <$ (runIO $ addEvent events CloseEvent)
-            , GLFW.setWindowSizeCallback $ runIO . addEvent events . ResizeEvent <$$> toSize
-            , GLFW.setWindowRefreshCallback $ runIO (addEvent events RefreshEvent)
-            , GLFW.setKeyCallback $ runIO . addEvent events <$$> ButtonEvent . KeyButton <%> btnState
-            , GLFW.setMouseButtonCallback $ runIO . addEvent events <$$> ButtonEvent . MouseButton <%> btnState
-            , GLFW.setMousePositionCallback $ runIO . addEvent events . MouseMoveEvent <$$> toPos
+        setCallbacks = sequence_
+            [ GLFW.setWindowCloseCallback $ True <$ (runIO $ addEvent CloseEvent)
+            , GLFW.setWindowSizeCallback $ runIO . addEvent . ResizeEvent <$$> toSize
+            , GLFW.setWindowRefreshCallback $ runIO (addEvent RefreshEvent)
+            , GLFW.setKeyCallback $ runIO . addEvent <$$> ButtonEvent . KeyButton <%> btnState
+            , GLFW.setMouseButtonCallback $ runIO . addEvent <$$> ButtonEvent . MouseButton <%> btnState
+            , GLFW.setMousePositionCallback $ runIO . addEvent . MouseMoveEvent <$$> toPos
             ]
 
--- | True if the maybe is Nothing, or the value it holds matches
-matchesMaybe :: Eq a => a -> Maybe a -> Bool
-matchesMaybe = maybe True . (==)
-
--- | Get a list of events matching any of the constraints, starting with the most recent
-poll :: TVar (Queue Event) -> EventPoller
-poll es constraints = atomically $ do
-        (es', ret) <- partition constraintMatch <$> toList <$> readTVar es
-        ret <$ writeTVar es (fromList es')
-    where
-        constraintMatch e = if (`any` constraints) $ matchesConstraint e
-                            then Right e
-                            else Left e
-
-        matchesConstraint (ButtonEvent s b) (ButtonEvents ms mb) = matchesMaybe s ms && matchesMaybe b mb
-        matchesConstraint (MouseMoveEvent _) MouseMoveEvents = True
-        matchesConstraint (ResizeEvent _) ResizeEvents = True
-        matchesConstraint CloseEvent CloseEvents = True
-        matchesConstraint RefreshEvent RefreshEvents = True
-        matchesConstraint _ _ = False
+popEvent :: IO (Maybe Event)
+popEvent = atomically $ tryReadTQueue events
